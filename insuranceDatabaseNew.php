f52d55cdb024a29687ab0ff2909a8694e6b96d8a
kevinyu0506 <kevinyu05062006@gmail.com>
2017-11-08 14:34:37 +0800

    完成公司選擇第一層

--- insuranceDatabaseNew.php
+++ insuranceDatabaseNew.php
@@ -63,8 +63,8 @@ td {
 </style>
 
 <head>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="css/fundsearch.css">
+    <!-- <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"> -->
+    <!-- <link rel="stylesheet" href="css/fundsearch.css"> -->
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
 </head>
@@ -91,36 +91,124 @@ td {
 <h3 align="center"><b>公司名稱</b></h3>
 <br>
 <hr>
-<form>
+<form id="insuranceSearch" name="insuranceSearch" method="post" action="insuranceResult.php">
 <table align="center">
     <tr>
-        <td><button class="myButton">台銀人壽</button></td>
-        <td><button class="myButton">台灣人壽</button></td>
-        <td><button class="myButton">保誠人壽</button></td>
-        <td><button class="myButton">國泰人壽</button></td>
-        <td><button class="myButton">中國人壽</button></td>
-        <td><button class="myButton">南山人壽</button></td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="a" onchange="companyFunc('台銀人壽');"/>台銀人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="b" onchange="companyFunc('台灣人壽');"/>台灣人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="c" onchange="companyFunc('保誠人壽');"/>保誠人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="d" onchange="companyFunc('國泰人壽');"/>國泰人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="e" onchange="companyFunc('中國人壽');"/>中國人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="f" onchange="companyFunc('南山人壽');"/>南山人壽
+            </label>
+        </td>
     </tr>
     <tr>
-        <td><button class="myButton">新光人壽</button></td>
-        <td><button class="myButton">富邦人壽</button></td>
-        <td><button class="myButton">遠雄人壽</button></td>
-        <td><button class="myButton">安聯人壽</button></td>
-        <td><button class="myButton">中華郵政</button></td>
-        <td><button class="myButton">全球人壽</button></td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="g" onchange="companyFunc('新光人壽');"/>新光人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="h" onchange="companyFunc('富邦人壽');"/>富邦人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="i" onchange="companyFunc('遠雄人壽');"/>遠雄人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="j" onchange="companyFunc('安聯人壽');"/>安聯人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="k" onchange="companyFunc('中華郵政');"/>中華郵政
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="l" onchange="companyFunc('全球人壽');"/>全球人壽
+            </label>
+        </td>
     </tr>
     <tr>
-        <td><button class="myButton">元大人壽</button></td>
-        <td><button class="myButton">康健人壽</button></td>
-        <td><button class="myButton">友邦人壽</button></td>
-        <td><button class="myButton">安達人壽</button></td>
-        <td><button class="myButton">第一金人壽</button></td>
-        <td><button class="myButton">法國巴黎人壽</button></td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="m" onchange="companyFunc('全球人壽');"/>全球人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="n" onchange="companyFunc('康健人壽');"/>康健人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="o" onchange="companyFunc('友邦人壽');"/>友邦人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="p" onchange="companyFunc('安達人壽');"/>安達人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="q" onchange="companyFunc('第一金人壽');"/>第一金人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="r" onchange="companyFunc('法國巴黎人壽');"/>法國巴黎人壽
+            </label>
+        </td>
     </tr>
     <tr>
-        <td><button class="myButton">蘇黎世人壽</button></td>
-        <td><button class="myButton">保德信人壽</button></td>
-        <td><button class="myButton">三商美邦人壽</button></td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="s" onchange="companyFunc('蘇黎世人壽');"/>蘇黎世人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="t" onchange="companyFunc('保德信人壽');"/>保德信人壽
+            </label>
+        </td>
+        <td>
+            <label class="myButton">
+                <input class="hidden" type="radio" name="company_name" id="u" onchange="companyFunc('三商美邦人壽');"/>三商美邦人壽
+            </label>
+        </td>
+        <td>
+            <input type="hidden" id="company" />
+            <input type="hidden" name="sql" id="sql">
+        </td>
     </tr>
     
 </table>
@@ -128,22 +216,22 @@ td {
 </div>
 <script type="text/javascript">
 
-function insuranceSearchSubmit() {
+function companyFunc(company){
+    document.getElementById('company').value = company;
+    // alert(document.getElementById('company').value);
 
-	sql = "";
+    sql = "";
 
-    sql = sql + "and insurance_premium.type ='" + document.getElementById("insuranceType").value + "' ";
-	sql = sql + "and insurance_premium.company = '" + document.getElementById("company").value + "' ";
-	sql = sql + "and insurance_premium.gender = '" + document.getElementById("gender").value + "' ";
-	sql = sql + "and insurance_premium.age = '" + document.getElementById("currentAge").value + "' ";
+    sql = sql + "and insurance_premium.company = '" + document.getElementById('company').value + "' ";
 
-	document.getElementById("sql").value = "SELECT insurance_premium.company,insurance_premium.name,insurance_premium.type,insurance_premium.currency,insurance_premium.condition,insurance_premium.unit,insurance_premium.gender,insurance_premium.age,insurance_premium.premium,insurance_premium.length FROM web_data.insurance_premium where 1=1 "+sql;
+    document.getElementById("sql").value = "SELECT distinct insurance_premium.name FROM web_data.insurance_premium where 1=1 "+sql;
 
-	document.getElementById("insuranceSearch").action = "insuranceResult.php";
-	document.getElementById("insuranceSearch").submit();
+    // alert(document.getElementById("sql").value);
 
-	// alert(document.getElementById("sql").value);
+    document.getElementById("insuranceSearch").action = "insuranceResultNew.php";
+    document.getElementById("insuranceSearch").submit();
 
 }
+
 
 </script>
---
+++ insuranceResultNew.php
@@ -0,0 +1,77 @@
+<?php 
+include "navbar.html";
+set_time_limit(0);
+mysql_connect("140.119.86.174", "nccu", "nccu"); //連結伺服器
+mysql_select_db("web_data"); //選擇資料庫
+mysql_query("set names utf8"); //以utf8讀取資料，讓資料可以讀取中文
+?>
+
+<style>
+div.tab {
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+}
+div.tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+    font-size: 17px;
+}
+div.tab button:hover {
+    background-color: #ddd;
+}
+div.tab button.active {
+    background-color: #ccc;
+}
+.tabcontent {
+    display: none;
+    padding: 6px ;
+    border: 1px solid #ccc;
+    border-top: none;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th,
+td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #f2f2f2
+}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
+</head>
+
+<div class="container">
+    <table>
+        <tr>
+            <th>商品</th>
+        </tr>
+<?php
+$sql=$_POST['sql'];
+$result=mysql_query("$sql");
+for($i=0;$i<mysql_num_rows($result);$i++){
+$rs=mysql_fetch_row($result);
+?>
+        <tr class="content">
+          <td><?php echo $rs[0]?></td>
+        </tr>
+<?php
+}
+?>
+    </table>
+</div>
