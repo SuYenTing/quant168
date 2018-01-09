67af7e2d4046ff8e7636f04a462bf4b447dc6865
chenhd1023 <chenhd1023@gmail.com>
2018-01-09 15:46:55 +0800

    201801091546

--- stockFeature/stockFeatureKbarList.php
+++ stockFeature/stockFeatureKbarList.php
@@ -15,16 +15,16 @@
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result1=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長紅貫穿%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs1=mysql_fetch_row($result1);
-?>
+$result1 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長紅貫穿%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs1 = mysql_fetch_row($result1);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs1[1];?></td>
-                        <td class="td"><?php echo $rs1[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs1[0] ?>"><?php echo $rs1[0] ?></a></td>
+                        <td class="td"><?php echo $rs1[1]; ?></td>
+                        <td class="td"><?php echo $rs1[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarGood1.php');">更多...</a></td>
                     </tr>
@@ -38,16 +38,16 @@ $result1=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result2=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%強旭日東昇%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs2=mysql_fetch_row($result2);
-?>
+$result2 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%強旭日東昇%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs2 = mysql_fetch_row($result2);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs2[1];?></td>
-                        <td class="td"><?php echo $rs2[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs2[0] ?>"><?php echo $rs2[0] ?></a></td>
+                        <td class="td"><?php echo $rs2[1]; ?></td>
+                        <td class="td"><?php echo $rs2[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarGood2.php');">更多...</a></td>
                     </tr>
@@ -63,16 +63,16 @@ $result2=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result3=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%光明在望%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs3=mysql_fetch_row($result3);
-?>
+$result3 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%光明在望%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs3 = mysql_fetch_row($result3);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs3[1];?></td>
-                        <td class="td"><?php echo $rs3[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs3[0] ?>"><?php echo $rs3[0] ?></a></td>
+                        <td class="td"><?php echo $rs3[1]; ?></td>
+                        <td class="td"><?php echo $rs3[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarGood3.php');">更多...</a></td>
                     </tr>
@@ -86,25 +86,25 @@ $result3=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result4=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長紅吞噬%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs4=mysql_fetch_row($result4);
-?>
+$result4 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長紅吞噬%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs4 = mysql_fetch_row($result4);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs4[1];?></td>
-                        <td class="td"><?php echo $rs4[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs4[0] ?>"><?php echo $rs4[0] ?></a></td>
+                        <td class="td"><?php echo $rs4[1]; ?></td>
+                        <td class="td"><?php echo $rs4[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarGood4.php');">更多...</a></td>
                     </tr>
                 </table>
             </td>
         </tr>
-        
-        
-        
+
+
+
     </table>
 </div>
 </div>
@@ -121,16 +121,16 @@ $result4=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result1=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長黑貫穿%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs1=mysql_fetch_row($result1);
-?>
+$result1 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長黑貫穿%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs1 = mysql_fetch_row($result1);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs1[1];?></td>
-                        <td class="td"><?php echo $rs1[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs1[0] ?>"><?php echo $rs1[0] ?></a></td>
+                        <td class="td"><?php echo $rs1[1]; ?></td>
+                        <td class="td"><?php echo $rs1[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarBad1.php');">更多...</a></td>
                     </tr>
@@ -144,16 +144,16 @@ $result1=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result2=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%強烏雲罩頂%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs2=mysql_fetch_row($result2);
-?>
+$result2 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%強烏雲罩頂%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs2 = mysql_fetch_row($result2);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs2[1];?></td>
-                        <td class="td"><?php echo $rs2[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs2[0] ?>"><?php echo $rs2[0] ?></a></td>
+                        <td class="td"><?php echo $rs2[1]; ?></td>
+                        <td class="td"><?php echo $rs2[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarBad2.php');">更多...</a></td>
                     </tr>
@@ -169,16 +169,16 @@ $result2=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result3=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%不懷好意%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs3=mysql_fetch_row($result3);
-?>
+$result3 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%不懷好意%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs3 = mysql_fetch_row($result3);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs3[1];?></td>
-                        <td class="td"><?php echo $rs3[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs3[0] ?>"><?php echo $rs3[0] ?></a></td>
+                        <td class="td"><?php echo $rs3[1]; ?></td>
+                        <td class="td"><?php echo $rs3[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarBad3.php');">更多...</a></td>
                     </tr>
@@ -192,25 +192,25 @@ $result3=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_da
                         <th class="th">強勢排名(  /1560)</th>
                     </tr>
 <?php
-$result4=mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長黑吞噬%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
-    for($i=1;$i<=3;$i++){
-        $rs4=mysql_fetch_row($result4);
-?>
+$result4 = mysql_query("SELECT stock_tech.code,today_price,order_angle FROM web_data.stock_tech where KBar_combination like'%長黑吞噬%' and stock_tech.date=(select max(stock_tech.date) from web_data.stock_tech) order by order_angle limit 3;");
+for ($i = 1; $i <= 3; $i++) {
+	$rs4 = mysql_fetch_row($result4);
+	?>
                     <tr class="tr">
-                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs[0] ?>"><?php echo $rs[0] ?></td>
-                        <td class="td"><?php echo $rs4[1];?></td>
-                        <td class="td"><?php echo $rs4[2];?></td>
+                        <td class="td"><a href="stockSearch.php?code=<?php echo $rs4[0] ?>"><?php echo $rs4[0] ?></a></td>
+                        <td class="td"><?php echo $rs4[1]; ?></td>
+                        <td class="td"><?php echo $rs4[2]; ?></td>
                     </tr>
-<?php } ?>
+<?php }?>
                     <tr class="tr">
                         <td colspan="3" style="text-align:right;color: orange;"><a href="javascript:moreInformSubmit('stockFeature/stockFeatureKbar/stockFeatureKbarBad4.php');">更多...</a></td>
                     </tr>
                 </table>
             </td>
         </tr>
-        
-        
-        
+
+
+
     </table>
 </div>
 </div>
